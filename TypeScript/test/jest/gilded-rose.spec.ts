6f9c53b606272cde06f873db94a99b2e4ce1abf8
André Brandão <andrenbrandao@gmail.com>
2023-11-18 19:57:46 -0300

    test: create tests for aged brie

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,23 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
+  describe("Aged Brie", () => {
+    describe('when sellIn is greater than zero', () => {
+      it('increases quality with time', () => {
+        const gildedRose = new GildedRose([new Item('Aged Brie', 1, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toBe(1);
+        expect(items[0].sellIn).toBe(0);
+      });
+    })
+
+    describe('when sellIn is zero', () => {
+      it('increases quality by 2', () => {
+        const gildedRose = new GildedRose([new Item('Aged Brie', 0, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toBe(2);
+        expect(items[0].sellIn).toBe(-1);
+      });
+    })
+  })
 });
