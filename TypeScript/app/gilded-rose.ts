11984448c7079dc818efe7c2c0b3672ed052802f
André Brandão <andrenbrandao@gmail.com>
2023-11-18 22:03:16 -0300

    refactor: group logic by item

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -32,29 +32,29 @@ export class GildedRose {
         if (this.items[i].sellIn < 6) {
           this.increaseItemQuality(i)
         }
+
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+
+        if (this.items[i].sellIn < 0) {
+          this.items[i].quality = 0
+        }
+        continue
       }
 
       if (this.items[i].name == 'Aged Brie') {
         this.increaseItemQuality(i)
-      }
 
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        this.decreaseItemQuality(i)
-      }
+        this.items[i].sellIn = this.items[i].sellIn - 1;
 
-      this.items[i].sellIn = this.items[i].sellIn - 1;
-
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name == 'Aged Brie') {
+        if (this.items[i].sellIn < 0) {
           this.increaseItemQuality(i)
-          continue
         }
+        continue
+      }
 
-        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          this.items[i].quality = 0
-          continue
-        }
-
+      this.decreaseItemQuality(i)
+      this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (this.items[i].sellIn < 0) {
         this.decreaseItemQuality(i)
       }
     }
