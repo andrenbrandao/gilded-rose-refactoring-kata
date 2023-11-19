37c499a1f40f93d88513e9afa82027b45460e651
André Brandão <andrenbrandao@gmail.com>
2023-11-18 21:14:51 -0300

    refactor: split quality operations by item type

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -23,25 +23,29 @@ export class GildedRose {
         continue
       }
 
+      if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1
+          if (this.items[i].sellIn < 11) {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1
+            }
+          }
+          if (this.items[i].sellIn < 6) {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1
+            }
+          }
+        }
+      }
+
+      if (this.items[i].name == 'Aged Brie' && this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1
+      }
+
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           this.items[i].quality = this.items[i].quality - 1
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
         }
       }
 
